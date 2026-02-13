467be8a74677968361201685550b2ccd1808f3c0
krunalc806-maker <krunalc806@gmail.com>
2026-02-13 16:23:22 +0530

    Add files via upload

---
+++ python.ai/python.py
@@ -0,0 +1,112 @@
+# variable
+
+# num = 20
+# print(num)
+
+# age = 19
+# print(age)
+
+# age = 18
+# name = "krunal"
+# print(age,name)
+
+# Name = input("enter your name:")
+# print(Name)
+
+# age = int(input("enter your age:"))
+# print(age + 10)
+
+
+# string
+
+# first = "ai"
+# second = "ml"
+# print(first +" "+second)
+
+# name = "krunal"
+# city = "ahmedabad"
+# job = "aiml specialist"
+# dream = "business"
+# print("hello",name)
+# print("i am living",city)
+# print("my shokh job",job)
+# # print("my dream is",dream)
+
+# name = "krunal"
+# age = 19
+# # print(f"my name is {name} and age is {age}")
+
+# name = "royalchavda"
+# city = "ahmedabad"
+# # print(f"my name is {name} and i am living in {city} ")
+
+# name = "Royalking"
+# age = 19
+# is_pass = True
+# percentage = 99.80
+# print("my name is",name,"and i am ",age,"years old and i am in 12",is_pass,"and my percentage is",percentage)
+
+# # type conversing
+# # string to int
+# age = int("19")
+# print(age)
+
+# # int  to string
+# age = 19
+# print("my age is" + str(age)) 
+
+# operater
+
+# arithmetic
+# a=30
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+#  comparison
+
+# age = 19
+# print(age >= 19)
+# print(age <= 19)
+# print(age == 19)
+# print(age != 19)
+# print(age < 19)
+# print(age > 19)
+
+# # logical
+
+# age = 19
+# has_id = True
+# print(age >= 19 and has_id)
+
+# name = "krunal"
+# age = 19
+# print(name or age)
+
+
+# is_logged_in = False
+# print(not is_logged_in)
+# not True ko False bana deta hai
+# False ko True
+
+
+# assignment
+# x=10
+# x+=10
+# print(x)
+
+# age=20 
+# if age >= 18:
+#     print("adult")
+# else:
+#     print("minor")
+
+# marks = 50
+# if marks>=55:
+#     print("pass")
+# else:
+#     print("fail")    
+
